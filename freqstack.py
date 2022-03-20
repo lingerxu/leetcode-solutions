27f0e2b28fbac5332f920fd6621333d3b5f46c10
Linger Xu <txu@iu.edu>
2022-03-19 19:47:22 -0700

    update daily challenge mar 19 2022

--- freqstack.py
+++ freqstack.py
@@ -1,8 +1,12 @@
+# daily challenge 03-19-2022
+import collections
 
 class FreqStack(object):
 
     def __init__(self):
-        self.freq
+        self.freq = collections.Counter()
+        self.group = collections.defaultdict(list)
+        self.maxfreq = 0
         
 
     def push(self, val):
@@ -10,9 +14,35 @@ class FreqStack(object):
         :type val: int
         :rtype: None
         """
+        valfreq = self.freq[val] + 1
+        self.freq[val] = valfreq
+        if valfreq > self.maxfreq:
+            self.maxfreq = valfreq
+        self.group[valfreq].append(val)
         
 
     def pop(self):
         """
         :rtype: int
         """
+        val = self.group[self.maxfreq].pop()
+        self.freq[val] -= 1
+        if not self.group[self.maxfreq]:
+            self.maxfreq -= 1
+
+        return val
+
+freqStack = FreqStack()
+freqStack.push(5)
+freqStack.push(7)
+freqStack.push(5)
+freqStack.push(7)
+freqStack.push(4)
+freqStack.push(5)
+freqStack.push(7)
+freqStack.pop()
+print(freqStack.group)
+print(freqStack.freq)
+# freqStack.pop()
+# freqStack.pop()
+# freqStack.pop()
