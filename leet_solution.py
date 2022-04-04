1bf4ea26c41399b3067155406e764bf2ad2566df
Linger Xu <txu@iu.edu>
2022-04-03 21:47:33 -0700

    twosum 2 and resersewords 3

--- leet_solution.py
+++ leet_solution.py
@@ -1,34 +1,58 @@
 class Solution(object):
-    def nextPermutation(self, nums):
+    def twoSum(self, numbers, target):
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
         """
+        # two pointers, from 0, n-1 to inward
         n = len(nums)
-        i = n - 2
-        # find the first number that is smaller than its right neighbor
-        while i >= 0 and nums[i+1] <= nums[i]:
-            i -= 1
-        # swap the earliest numnber that's smaller with the number at current i
-        if i >= 0:
-            j = n - 1
-            while nums[j] <= nums[i]:
-                j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
+        left = 0
+        right = n - 1
+        while left < right:
+            tmpsum = nums[left] + nums[right]
+            if tmpsum == target:
+                return [left, right]
+            elif tmpsum < target:
+                left += 1
+            else: # tmpsum > target:
+                right -= 1
 
-        # reserve the sequence after i+1
-        # since we already know that its in descending order from i+1
-        a = i + 1
-        b = n - 1
-        while a < b:
-            nums[a], nums[b] = nums[b], nums[a]
-            a += 1
-            b -= 1
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # n = len(s)
+        # sub_start = 0
+        # sub_end = 0
+        # idx_blank = 0
+        # result = ""
+        # for i in range(n):
+        #     if s[i] == " " or i == n - 1:
+        #         if idx_blank > 0:
+        #             sub_start = idx_blank + 1
+        #         idx_blank = i
+        #         sub_end = idx_blank - 1 if i < n - 1 else i
+        #         # reverse the string between sub_start and sub_end
+        #         # and add to result
+        #         sub_str = ""
+        #         while sub_end >= sub_start:
+        #             sub_str += s[sub_end]
+        #             sub_end -= 1
+        #         if i < n - 1:
+        #             result = result + sub_str + " "
+        #         else:
+        #             result += sub_str
 
-        return nums
+        # return result
+        return ' '.join(x[::-1] for x in s.split())
+
 
 sol = Solution()
-nums = [1, 2, 3, 5, 4]
+nums = [2,7,11,15]
+target = 9
+s = "Let's take LeetCode contest"
 
-result = sol.nextPermutation(nums)
+result = sol.reverseWords(s)
 print(result)
